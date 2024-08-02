38f118500a8a0402f5898fe6beab452f3c6a9335
Emir Keles <burhanemirkeles@outlook.com>
2024-08-03 01:59:58 +0300

    add filtersView component

---
+++ eteration/eteration/Views/FiltersView.swift
@@ -0,0 +1,72 @@
+//
+//  FiltersView.swift
+//  eteration
+//
+//  Created by Emir Keleş on 3.08.2024.
+//
+
+import UIKit
+
+public final class FiltersView: UIView {
+  public struct Item {
+    let onTapButton: () -> Void
+    
+    init(onTapButton: @escaping () -> Void) {
+      self.onTapButton = onTapButton
+    }
+  }
+
+  private var item: Item?
+
+  private let filtersLabel: UILabel = {
+    let label = UILabel()
+    label.text = "Filters:"
+    label.font = UIFont.systemFont(ofSize: 16)
+    label.translatesAutoresizingMaskIntoConstraints = false
+    return label
+  }()
+
+  private let selectFilterButton: UIButton = {
+    let button = UIButton(type: .system)
+    button.setTitle("Select Filter", for: .normal)
+    button.backgroundColor = UIColor.lightGray
+    button.setTitleColor(.black, for: .normal)
+    button.layer.cornerRadius = 6
+    button.translatesAutoresizingMaskIntoConstraints = false
+    return button
+  }()
+
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    setupView()
+    configureButton()
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  private func configureButton() {
+    selectFilterButton.addTarget(self, action: #selector(onTapSelectFilterButton), for: .touchUpInside)
+  }
+
+  private func setupView() {
+    addSubview(filtersLabel)
+    addSubview(selectFilterButton)
+
+    NSLayoutConstraint.activate([
+      filtersLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
+      filtersLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
+
+      selectFilterButton.leadingAnchor.constraint(equalTo: filtersLabel.trailingAnchor, constant: 10),
+      selectFilterButton.trailingAnchor.constraint(equalTo: trailingAnchor),
+      selectFilterButton.centerYAnchor.constraint(equalTo: centerYAnchor),
+      selectFilterButton.widthAnchor.constraint(equalToConstant: 158),
+      selectFilterButton.heightAnchor.constraint(equalToConstant: 36)
+    ])
+  }
+
+  @objc private func onTapSelectFilterButton() {
+    print("Tapped Button")
+  }
+}
