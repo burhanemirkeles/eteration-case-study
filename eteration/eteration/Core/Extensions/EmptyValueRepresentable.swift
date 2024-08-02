f9137899d87a938b05c409e314764978ceaedf3b
Emir Keles <burhanemirkeles@outlook.com>
2024-08-03 01:21:09 +0300

    add basic extensions

---
+++ eteration/eteration/Core/Extensions/EmptyValueRepresentable.swift
@@ -0,0 +1,74 @@
+//
+//  EmptyValueRepresentable.swift
+//  eteration
+//
+//  Created by Emir Keleş on 3.08.2024.
+//
+
+import Foundation
+
+public protocol EmptyValueRepresentable {
+  static var emptyValue: Self { get }
+}
+
+extension String: EmptyValueRepresentable {
+  public static var emptyValue: String { return "" }
+}
+
+extension Array: EmptyValueRepresentable {
+  public static var emptyValue: [Element] { return [] }
+}
+
+// MARK: Boolean Optionals
+public extension Swift.Optional where Wrapped == Bool {
+  var orTrue: Bool {
+    guard let self = self else { return true }
+    return self
+  }
+
+  var orFalse: Bool {
+    guard let self = self else { return false }
+    return self
+  }
+
+  func isEqualTo(_ bool: Bool?) -> Bool {
+    guard let self = self, let boolValue = bool else { return false }
+    return self == boolValue
+  }
+}
+
+// MARK: Integer Optional
+public extension Swift.Optional where Wrapped == Int {
+  func or(_ defaultValue: Int) -> Int {
+    guard let self = self else { return defaultValue }
+    return self
+  }
+}
+
+// MARK: Nil Check
+public extension Swift.Optional {
+  var notNil: Bool { return self != nil }
+  var isNil: Bool { return self == nil }
+}
+
+// MARK: String
+public extension Swift.Optional where Wrapped == String {
+  var isNilOrEmpty: Bool {
+    guard let self = self else { return true }
+    return self.isEmpty
+  }
+}
+
+public extension Swift.Optional where Wrapped == String {
+  func or(_ defaultValue: String) -> String {
+    guard let self = self, !self.isEmpty else { return defaultValue }
+    return self
+  }
+
+  func isEqualTo(_ string: String?) -> Bool {
+    guard let self = self, let string = string else { return false }
+    return self == string
+  }
+}
+
+
