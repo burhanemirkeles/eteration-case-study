354eee0effeb811ef86b71959178d9f2699dc1bf
Emir Keles <burhanemirkeles@outlook.com>
2024-08-02 23:31:24 +0300

    init project

--- eteration/eteration/ViewController.swift
+++ eteration/eteration/ViewController.swift
@@ -11,9 +11,20 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
-  }
 
+    view.backgroundColor = .white
+
+    let label = UILabel()
+    label.text = "Hello, eteration!"
+    label.textAlignment = .center
+    label.translatesAutoresizingMaskIntoConstraints = false
+    view.addSubview(label)
+
+    NSLayoutConstraint.activate([
+      label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+    ])
+  }
 
 }
 
