df595bb1f450c01bca23ba20d9eea888bd34072d
Emir Keles <burhanemirkeles@outlook.com>
2024-08-03 02:00:50 +0300

    add views to homeViewController

--- eteration/eteration/ViewControllers/HomeViewController.swift
+++ eteration/eteration/ViewControllers/HomeViewController.swift
@@ -10,8 +10,21 @@ import UIKit
 class HomeViewController: UIViewController {
   private var viewModel: HomeViewModel
 
+  // MARK: - Subviews
+  private let customSearchBar: SearchBar = {
+    let searchBar = SearchBar()
+    searchBar.translatesAutoresizingMaskIntoConstraints = false
+    return searchBar
+  }()
+
+  private let filtersView: FiltersView = {
+    let view = FiltersView()
+    view.translatesAutoresizingMaskIntoConstraints = false
+    return view
+  }()
+
   init() {
-    let model = HomeModel(title: nil)
+    let model = HomeModel(title: "Home Page")
     self.viewModel = HomeViewModel(model: model)
     super.init(nibName: nil, bundle: nil)
   }
@@ -22,19 +35,31 @@ class HomeViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-
     view.backgroundColor = .white
-    title = viewModel.title
+    setupSearchBar()
+    setupFiltersView()
+  }
 
-    let label = UILabel()
-    label.text = viewModel.title
-    label.textAlignment = .center
-    label.translatesAutoresizingMaskIntoConstraints = false
-    view.addSubview(label)
+  // MARK: - Configure Subviews
+  private func setupSearchBar() {
+    view.addSubview(customSearchBar)
 
     NSLayoutConstraint.activate([
-      label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-      label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+      customSearchBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
+      customSearchBar.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+      customSearchBar.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+      customSearchBar.heightAnchor.constraint(equalToConstant: 40)
+    ])
+  }
+
+  private func setupFiltersView() {
+    view.addSubview(filtersView)
+
+    NSLayoutConstraint.activate([
+      filtersView.topAnchor.constraint(equalTo: customSearchBar.bottomAnchor, constant: 20),
+      filtersView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+      filtersView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+      filtersView.heightAnchor.constraint(equalToConstant: 36)
     ])
   }
 }
